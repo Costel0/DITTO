398bb808217b083bb81a7c5e1964338e79833edb
Costel0 <familiamiralpeixmercadal@gmail.com>
2026-08-10 21:50:56 +0200

    Add welcome screen

---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+import '../auth/session_controller.dart';
+import '../navigation/app_routes.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({
+    super.key,
+    required this.sessionController,
+  });
+
+  final SessionController sessionController;
+
+  void _logout(BuildContext context) {
+    sessionController.clearSession();
+    Navigator.of(context).pushReplacementNamed(AppRoutes.login);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.all(24),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Container(
+                      width: 64,
+                      height: 64,
+                      decoration: BoxDecoration(
+                        color: theme.colorScheme.primaryContainer,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Icon(
+                        Icons.waving_hand_rounded,
+                        color: theme.colorScheme.primary,
+                        size: 30,
+                      ),
+                    ),
+                    const SizedBox(height: 24),
+                    Text(
+                      'Hello ${sessionController.username}',
+                      textAlign: TextAlign.center,
+                      style: theme.textTheme.headlineLarge?.copyWith(
+                        fontWeight: FontWeight.w700,
+                        letterSpacing: -1,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Positioned(
+              top: 12,
+              right: 12,
+              child: TextButton.icon(
+                onPressed: () => _logout(context),
+                icon: const Icon(Icons.logout_rounded),
+                label: const Text('Log out'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
