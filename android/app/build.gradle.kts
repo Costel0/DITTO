1f30d8b895d9b5f5e4d74c10642d1482ff949814
Costel0 <familiamiralpeixmercadal@gmail.com>
2026-08-15 19:20:29 +0200

    Align Android minimum SDK with Flutter support

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.ditto"
-        minSdk = 23
+        minSdk = 24
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
