b950b51a506803d712a377a12386509150996d45
Costel0 <familiamiralpeixmercadal@gmail.com>
2026-08-21 13:25:08 +0200

    Update garden task registry test

--- test/features/jobs/domain/job_task_test.dart
+++ test/features/jobs/domain/job_task_test.dart
@@ -3,12 +3,20 @@ import 'package:ditto/features/jobs/domain/job_task.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('garden task registry contains prepare and upgrade tasks', () {
+  test('garden task registry contains prepare and chained upgrade tasks', () {
     final gardenTasks = jobTasksForArea(JobArea.garden);
 
     expect(
       gardenTasks.map((task) => task.id),
-      <String>['prepare_garden', 'upgrade_garden'],
+      <String>[
+        'prepare_garden',
+        'upgrade_garden',
+        'upgrade_garden_2',
+        'upgrade_garden_3',
+        'upgrade_garden_4',
+        'upgrade_garden_5',
+        'upgrade_garden_6',
+      ],
     );
   });
 
