5958a4947c47dbba1221a8a09add9944a3f6de05
Costel0 <familiamiralpeixmercadal@gmail.com>
2026-09-09 16:28:24 +0200

    Expose expedition review flow

--- lib/features/expeditions/domain/expedition_service.dart
+++ lib/features/expeditions/domain/expedition_service.dart
@@ -1,7 +1,12 @@
 import 'expedition.dart';
+import 'expedition_review.dart';
 
 abstract interface class ExpeditionService {
   Future<ExpeditionLauncherInfo> fetchLauncherInfo();
+
+  Future<List<ExpeditionReview>> fetchPendingReviews();
+
+  Future<ExpeditionReview> reviewExpeditionResult(String reviewId);
 
   Future<void> startExpedition({
     required List<String> survivorIds,
