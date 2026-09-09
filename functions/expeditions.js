ddde59b66f9d524e59583374674875e3940f2dba
Costel0 <familiamiralpeixmercadal@gmail.com>
2026-09-09 16:51:53 +0200

    Expose expedition inventory reward helper

--- functions/expeditions.js
+++ functions/expeditions.js
@@ -459,6 +459,7 @@ module.exports = {
   actionIdsFromExpeditionTaskId,
   aggregateExpeditionInventoryDelta,
   applyExpeditionCompletion,
+  applyInventoryReward,
   availableActionsAtCoordinates,
   canonicalActionId,
   coordinatesFromExpeditionLocation,
