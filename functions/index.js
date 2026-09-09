2472d0ea3c02bd64892c455268e209b8b22fddaf
Costel0 <familiamiralpeixmercadal@gmail.com>
2026-09-09 13:35:45 +0200

    Remove obsolete expedition coordinate import

--- functions/index.js
+++ functions/index.js
@@ -29,7 +29,6 @@ const {
   actionIdsFromExpeditionTaskId,
   applyExpeditionCompletion,
   availableActionsAtCoordinates,
-  coordinatesFromExpeditionLocation,
   expeditionDefinitionFromSnapshot,
   expeditionDurationSeconds,
   expeditionEnergyDelta,
