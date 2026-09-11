49d69b0a878ce0aa763a4a1c17d601df21013ac6
Costel0 <familiamiralpeixmercadal@gmail.com>
2026-09-11 13:38:06 +0200

    Remove redundant PowerShell map edit wrapper

--- map-edit.ps1
+++
@@ -1,9 +0,0 @@
-$ErrorActionPreference = "Stop"
-Push-Location (Join-Path $PSScriptRoot "functions")
-try {
-  & node "scripts/map_edit.js" @args
-  exit $LASTEXITCODE
-}
-finally {
-  Pop-Location
-}
