dcc0df0b7747bd6c46ac5d615df04f87168806dd
Costel0 <familiamiralpeixmercadal@gmail.com>
2026-09-11 16:22:32 +0200

    Fix expedition coordinate rejection test

--- functions/test/expeditions.test.js
+++ functions/test/expeditions.test.js
@@ -111,7 +111,7 @@ test("expedition actions are rejected outside their available coordinates", () =
   assert.throws(
     () => selectedActionDefinitions(
       definition,
-      {x: 0, y: 0, z: 0},
+      {x: 0, y: 1, z: 1},
       {x: 4, y: 5, z: 6},
       ["inspect"],
     ),
