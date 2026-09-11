eeb7f9de62d9080ee0e99f3eb8cb63be58bdc332
Costel0 <familiamiralpeixmercadal@gmail.com>
2026-09-11 13:37:28 +0200

    Route manual map edits through existing map command

--- map.cmd
+++ map.cmd
@@ -1,7 +1,13 @@
 @echo off
 setlocal
 pushd "%~dp0functions"
+if /I "%~1"=="set-sector" goto MAP_EDIT
+if /I "%~1"=="set-zone" goto MAP_EDIT
 node scripts\map_admin.js %*
+goto DONE
+:MAP_EDIT
+node scripts\map_edit.js %*
+:DONE
 set EXIT_CODE=%ERRORLEVEL%
 popd
 exit /b %EXIT_CODE%
